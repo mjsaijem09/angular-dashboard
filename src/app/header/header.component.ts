bd953d0a0b7e59e86ac21a58103ba73493fdfe51
mjsaijem09 <mejiasmark9@gmail.com>
2020-10-23 19:36:29 +0800

    3 files modified

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,6 +12,28 @@ export class HeaderComponent implements OnInit {
   @Input() deviceLg: boolean;
   @Input() deviceXl: boolean;
 
+  navs = [
+    { title: "Dashboard", icon: "dashboard", path: "javascript:;" },
+    { title: "Calendar", icon: "date_range", path: "javascript:;" },
+    { title: "Sales", icon: "trending_up", path: "javascript:;" },
+    { title: "Client", icon: "home_repair_service", path: "javascript:;" },
+    { title: "Staff", icon: "group", path: "javascript:;" },
+    { title: "Services", icon: "shopping_basket", path: "javascript:;" },
+    { title: "Resources", icon: "widgets", path: "javascript:;" },
+    { title: "Inventory", icon: "assignment", path: "javascript:;" },
+    { title: "Analytiics", icon: "leaderboard", path: "javascript:;" },
+    { title: "Setup", icon: "settings", path: "javascript:;" }
+  ];
+
+  status: boolean = false;
+  showSidebar() {
+    this.status = !this.status;
+  }
+
+  hideSidebar() {
+    this.status = !this.status;
+  }
+
   constructor() {}
 
   ngOnInit() {}
